79c62f9475e55b754c098658d30f489ed02fda09
Max <maksymgulenko@gmail.com>
2025-08-18 21:30:34 +0200

    else if added

--- IfKeywordAndCodeBlock/src/Main.java
+++ IfKeywordAndCodeBlock/src/Main.java
@@ -7,8 +7,10 @@ public class Main {
 
 
 
-        if (score <= 5000) {
-            System.out.println("Your score was less than or equal 5000");
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000");
         } else {
             System.out.println("Got here");
         }
