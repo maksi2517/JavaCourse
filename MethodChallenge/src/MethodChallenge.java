7bf01f5461b54d29ff5e87fe6ea1e8e0871ad77d
Max <maksymgulenko@gmail.com>
2025-08-24 19:39:06 +0200

    compare with

--- MethodChallenge/src/MethodChallenge.java
+++ MethodChallenge/src/MethodChallenge.java
@@ -3,27 +3,28 @@ public class MethodChallenge {
     public static void main(String[] args) {
 
         int highScorePosition = calculateHighScorePosition(1500);
-        displayHighScoreScore("Max",highScorePosition);
+        displayHighScorePosition("Tim", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(999);
-        displayHighScoreScore("Bob",highScorePosition);
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Bob", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(499);
-        displayHighScoreScore("Percy",highScorePosition);
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Percy", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(99);
-        displayHighScoreScore("Gilbert",highScorePosition);
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Gilbert", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(-1000);
-        displayHighScoreScore("James",highScorePosition);
-
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("James", highScorePosition);
     }
 
-    public static void displayHighScoreScore(String playerName, int highScorePosition) {
-        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score list");
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+                + highScorePosition + " on the high score list");
     }
 
-    public static int calculateHighScorePosition (int playerScore) {
+    public static int calculateHighScorePosition(int playerScore) {
+
         int position = 4;
         if (playerScore >= 1000) {
             position = 1;
@@ -32,9 +33,7 @@ public class MethodChallenge {
         } else if (playerScore >= 100) {
             position = 3;
         }
+
         return position;
-
     }
-
-
 }
