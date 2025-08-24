6cea5c4509c27c0a396d6141c79eaa66e4d48f2d
Max <maksymgulenko@gmail.com>
2025-08-24 19:27:07 +0200

    Challenge complete

---
+++ MethodChallenge/src/MethodChallenge.java
@@ -0,0 +1,39 @@
+public class MethodChallenge {
+
+    public static void main(String[] args) {
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScoreScore("Max",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(999);
+        displayHighScoreScore("Bob",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(499);
+        displayHighScoreScore("Percy",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(99);
+        displayHighScoreScore("Gilbert",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(-1000);
+        displayHighScoreScore("James",highScorePosition);
+
+    }
+
+    public static void displayHighScoreScore(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition (int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+
+}
