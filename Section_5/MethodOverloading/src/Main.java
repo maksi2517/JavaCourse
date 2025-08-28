d14a32db53c8b4d42ef9ca2925e0c161ff9e2d6c
Max <maksymgulenko@gmail.com>
2025-08-29 00:23:35 +0200

    end

--- Section_5/MethodOverloading/src/Main.java
+++ Section_5/MethodOverloading/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        /*
         int newScore = calculateScore("Max", 500);
         System.out.println("New Score is " + newScore);
 
         calculateScore(75);
         calculateScore();
+        */
+
+        System.out.println("New Score is " + calculateScore("Max", 500));
+        System.out.println("New Score is " + calculateScore(10));
+
     }
 
     public static int calculateScore (String playerName, int score) {
@@ -14,12 +20,18 @@ public class Main {
     }
 
     public static int calculateScore (int score) {
-        System.out.println("Unnamed Player scored " + score + " points");
-        return score * 1000;
+        return calculateScore ("Anonymous", score);
     }
 
-    public static int calculateScore (){
+    public static int calculateScore () {
         System.out.println("No player name, no player score");
         return 0;
     }
+
+    /*
+    kommt error
+    public static void calculateScore (){
+        System.out.println("No player name, no player score");
+    }
+    */
 }
