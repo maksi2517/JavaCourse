16fc4778123f1d700a0f1c2218103ea57df5e1c0
Max <maksymgulenko@gmail.com>
2025-08-30 12:24:41 +0200

    Switch statement

---
+++ Section_6/Switch/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+    /*
+        int value = 3;
+        if  (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+    */
+
+        int switchValue = 5;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 or 2");
+                break;
+        }
+    }
+}
