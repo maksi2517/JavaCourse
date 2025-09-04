7690d7ebcbcad1d038e63967916baaf1ee3f589d
Max <maksymgulenko@gmail.com>
2025-09-04 19:16:08 +0200

    while and do while loop

---
+++ Section_6/65_WhileLoop/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; ++i) {
+            System.out.println(i);
+        }
+
+        int j = 1;
+        boolean isReady = false;
+        do {
+            if (j > 5) {
+                break;
+            }
+            System.out.println(j);
+            j++;
+            isReady = (j > 0);
+        } while (isReady);
+
+    }
+
+}
