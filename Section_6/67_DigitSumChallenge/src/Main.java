cf46df5e340eb8d40bd306cf8534cb721f9d4c71
Max <maksymgulenko@gmail.com>
2025-09-04 20:10:29 +0200

    67 solved

---
+++ Section_6/67_DigitSumChallenge/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDigits(100));
+
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+}
