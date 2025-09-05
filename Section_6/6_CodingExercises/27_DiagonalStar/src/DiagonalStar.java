0f754921f91198853134beb971573e297cfcd75f
Max <maksymgulenko@gmail.com>
2025-09-05 12:27:45 +0200

    27 solved

---
+++ Section_6/6_CodingExercises/27_DiagonalStar/src/DiagonalStar.java
@@ -0,0 +1,26 @@
+public class DiagonalStar {
+    public static void main(String[] args) {
+        printSquareStar(5);
+        printSquareStar(8);
+        printSquareStar(18);
+    }
+
+    public static void printSquareStar (int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= number; col++) {
+                if (row == 1 || row == number || col == 1 || col == number || row == col || col == number - row + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // move to next line after each row
+        }
+    }
+
+}
