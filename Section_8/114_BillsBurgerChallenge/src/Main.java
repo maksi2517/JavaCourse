544420d71db77445d0c7cf443345c03cf1a987b7
Max <maksymgulenko@gmail.com>
2025-10-08 13:00:44 +0200

    115 done

--- Section_8/114_BillsBurgerChallenge/src/Main.java
+++ Section_8/114_BillsBurgerChallenge/src/Main.java
@@ -1,13 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        Item coke = new Item("drink", "coke", 1.5);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
 
-        Item avocado = new Item("Topping", "avocado", 1.5);
-        avocado.printItem();
+//        Item coke = new Item("drink", "coke", 1.5);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.5);
+//        avocado.printItem();
+
+//        Burger burger = new Burger("regular", 4.0);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE","MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
 
     }
 }
