0154889e28f17924d6a3dd706d0a40e5ef907a07
Max <maksymgulenko@gmail.com>
2025-10-08 12:30:19 +0200

    114 done

---
+++ Section_8/114_BillsBurgerChallenge/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Item coke = new Item("drink", "coke", 1.5);
+        coke.printItem();
+        coke.setSize("LARGE");
+        coke.printItem();
+
+        Item avocado = new Item("Topping", "avocado", 1.5);
+        avocado.printItem();
+
+    }
+}
