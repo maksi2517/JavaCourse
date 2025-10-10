1e01343556dbf924974dbf8d7515f39b98e005f3
Max <maksymgulenko@gmail.com>
2025-10-10 12:11:32 +0200

    43 done

---
+++ Section_9/9_CodingExercises/43_Reverse_An_Array/src/dev/lpa/Main.java
@@ -0,0 +1,24 @@
+package dev.lpa;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void reverse (int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+
+        System.out.println("Reversed array = " + Arrays.toString(array));
+
+    }
+}
