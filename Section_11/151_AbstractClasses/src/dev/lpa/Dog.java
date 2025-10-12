05f7b86ed4681c695e83c19ae5e37ebc12883ad8
Max <maksymgulenko@gmail.com>
2025-10-12 16:57:59 +0200

    152 done

--- Section_11/151_AbstractClasses/src/dev/lpa/Dog.java
+++ Section_11/151_AbstractClasses/src/dev/lpa/Dog.java
@@ -10,10 +10,22 @@ public class Dog extends Animal {
     @Override
     public void move(String speed) {
 
+        if(speed.equals("slow")) {
+            System.out.println(type + " walking");
+        } else {
+            System.out.println(type + " running");
+        }
+
     }
 
     @Override
     public void makeNoise() {
+
+        if (type == "Wolf") {
+            System.out.print("Howling! ");
+        } else {
+            System.out.print("Woof! ");
+        }
 
     }
 }
