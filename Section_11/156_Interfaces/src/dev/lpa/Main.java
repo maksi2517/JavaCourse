544618dd76481addaa1c2d147021bd5fd7272aac
Max <maksymgulenko@gmail.com>
2025-10-12 18:19:32 +0200

    157 done

--- Section_11/156_Interfaces/src/dev/lpa/Main.java
+++ Section_11/156_Interfaces/src/dev/lpa/Main.java
@@ -14,10 +14,29 @@ public class Main {
 //        flier.move();
 //        tracker.move();
 
+//        flier.takeOff();
+//        flier.fly();
+//        tracked.track();
+//        flier.land();
+
+        inFlight(flier);
+        inFlight(new Jet());
+        Trackable truck = new Truck();
+        truck.track();
+
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n",
+                kmsTraveled, milesTraveled);
+
+    }
+
+    private static void inFlight(FlightEnabled flier) {
         flier.takeOff();
         flier.fly();
-        tracked.track();
+        if (flier instanceof Trackable tracked) {
+            tracked.track();
+        }
         flier.land();
-
     }
 }
