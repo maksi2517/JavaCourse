3207b583863299b5ee26cf6c0146aeeb4c101a35
Max <maksymgulenko@gmail.com>
2025-10-12 17:46:29 +0200

    155 done

--- Section_11/154_AbstractChallenge/src/dev/lpa/Store.java
+++ Section_11/154_AbstractChallenge/src/dev/lpa/Store.java
@@ -2,6 +2,8 @@ package dev.lpa;
 
 import java.util.ArrayList;
 
+record OrderItem (int qty, ProductForSale product) { }
+
 public class Store {
 
     private static ArrayList<ProductForSale> storeProducts = new ArrayList<>();
@@ -12,8 +14,25 @@ public class Store {
                 "Impressionistic work by ABF painted in 2010"));
         storeProducts.add(new ArtObject("Sculpture", 2000,
                 "Bronze sculpture by JKF, produced in 1950"));
+        storeProducts.add(new Furniture("Desk", 500,
+                "Mahogany Desk"));
+        storeProducts.add(new Furniture("Lamp", 200,
+                "Tiffany Lamp with Hummingbirds"));
 
         listProducts();
+
+        System.out.println("\nOrder 1");
+        var order1 = new ArrayList<OrderItem>();
+        addItemToOrder(order1, 1, 2);
+        addItemToOrder(order1, 0, 1);
+        printOrder(order1);
+
+        System.out.println("\nOrder 2");
+        var order2 = new ArrayList<OrderItem>();
+        addItemToOrder(order2, 3, 5);
+        addItemToOrder(order2, 0, 1);
+        addItemToOrder(order2, 2, 1);
+        printOrder(order2);
     }
 
     public static void listProducts() {
@@ -21,5 +40,19 @@ public class Store {
             System.out.println("-".repeat(30));
             item.showDetails();
         }
+    }
+
+    public static void addItemToOrder (ArrayList<OrderItem> order, int orderIndex, int qty) {
+        order.add(new OrderItem(qty, storeProducts.get(orderIndex)));
+    }
+
+    public static void printOrder (ArrayList<OrderItem> order) {
+        double salesTotal = 0;
+        System.out.println("Your order contains:");
+        for (var item : order) {
+            item.product().printPricedItem(item.qty());
+            salesTotal += item.product().getSalePrice(item.qty());
+        }
+        System.out.printf("Sales Total: $%6.2f %n", salesTotal);
     }
 }
